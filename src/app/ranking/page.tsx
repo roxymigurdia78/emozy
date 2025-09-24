a837eb3aa9f72302778e7a81b7e64a5c4ae141a3
roxymigurdia78 <yoshifumikurosu1547@gmail.com>
2025-09-24 11:59:45 +0900

    vercelltest

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -3,7 +3,23 @@ import Link from "next/link";
 export default function profile() {
   return (
     <div>
-
+    <header style={{
+        backgroundColor:"#7ADAD5",
+        height:"100px",
+        display:"flex",
+        alignItems: "center"      
+    }}>
+        <Link href="/home" >
+            <Image 
+                src="/images/title.png"
+                alt="title"
+                width={150}
+                height={150}
+                style={{marginLeft: "5%"}}
+            />
+        </Link>    
+    </header>
+    
     <footer style={{
         backgroundColor:"#7ADAD5",
         height:"100px",
