9ad059becdf21a226d541a7a0fb53c0d488e3d15
roxymigurdia78 <yoshifumikurosu1547@gmail.com>
2025-09-26 15:41:53 +0900

    footer修正

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -67,66 +67,66 @@ export default function page() {
     </main>
 
 
-    <footer style={{
-        backgroundColor:"#7ADAD5",
-        height:"100px",
-        display:"flex",
+            <footer style={{
+        backgroundColor: "#f3f2f2ac",
+        height: "75px",
+        display: "flex",
         alignItems: "center",
-        justifyContent: "center",
-        position:"fixed",
-        bottom:"0",
-        width:"100%"
+        justifyContent: "space-between",
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        padding: "0 32px"
     }}>
-        <Link href="/home">
-            <Image
-                src=""
-                alt="homeicon"
-                width={50}
-                height={50}
-                style={{}}
-            />
-        </Link>
-
-        <Link href="/ranking">
-            <Image
-                src=""
-                alt="rankingicon"
-                width={50}
-                height={50}
-                style={{}}
-            />
-        </Link>
-
-        <Link href="/post">
-            <Image
-                src=""
-                alt="posticon"
-                width={50}
-                height={50}
-                style={{}}
-            />
-        </Link>
-
-        <Link href="/search">
-            <Image
-                src=""
-                alt="searchicon"
-                width={50}
-                height={50}
-                style={{}}
-            />
-        </Link>
-
-        <Link href="/profile">
-            <Image
-                src=""
-                alt="profileicon"
-                width={50}
-                height={50}
-                style={{}}
-            />
-        </Link>
+    <Link href="/home" style={{ display: "flex", alignItems: "flex-end", height: "100px", flexShrink: 0, flexGrow: 0 }}>
+        <Image
+            src="/images/homeicon.png"
+            alt="homeicon"
+            width={60}
+            height={60}
+            style={{ marginLeft: "-30px", marginTop: "10px", marginBottom: "15px", marginRight: "3px", minWidth: "65px", minHeight: "65px" }}
+        />
+    </Link>
+    <Link href="/ranking" style={{ display: "flex", alignItems: "flex-end", height: "100px", flexShrink: 0, flexGrow: 0 }}>
+        <Image
+            src="/images/rankingicon.png"
+            alt="rankingicon"
+            width={60}
+            height={60}
+            style={{ marginLeft: "0px", marginTop: "10px", marginBottom: "15px", marginRight: "0px", minWidth: "65px", minHeight: "65px" }}
+        />
+    </Link>
+    <Link href="post" style={{ display: "flex", alignItems: "flex-end", height: "100px", flexShrink: 0, flexGrow: 0 }}>
+        <Image
+            src="/images/toukouicon.png"
+            alt="posticon"
+            width={60}
+            height={60}
+            style={{ marginLeft: "0px", marginTop: "10px", marginBottom: "19px", marginRight: "0px", minWidth: "65px", minHeight: "65px" }}
+        />
+    </Link>
+    <Link href="/search" style={{ display: "flex", alignItems: "flex-end", height: "100px", flexShrink: 0, flexGrow: 0 }}>
+        <Image
+            src="/images/searchicon.png"
+            alt="searchicon"
+            width={60}
+            height={60}
+            style={{ marginLeft: "0px", marginTop: "10px", marginBottom: "22px", marginRight: "-5px", minWidth: "65px", minHeight: "65px" }}
+        />
+    </Link>
+    <Link href="/profile" style={{ display: "flex", alignItems: "flex-end", height: "100px", flexShrink: 0, flexGrow: 0 }}>
+        <Image
+            src="/images/toukouicon.png"
+            alt="profileicon"
+            width={60}
+            height={60}
+            style={{ marginLeft: "-8px", marginTop: "10px", marginBottom: "19px", marginRight: "-24px", minWidth: "65px", minHeight: "65px" }}
+        />
+    </Link>
     </footer>
+
+
+
 
     </div>
   )
